399e1ee03ce34035b892b3f29394e037d4e03c54
niros <niro655@gmail.com>
2022-01-18 19:42:17 +0200

    update server base url for gov

--- src/services/govApi.js
+++ src/services/govApi.js
@@ -1,5 +1,5 @@
 import {api} from './http';
 
-const baseUrl = 'http://localhost:2400/api/gov';
+const baseUrl = 'https://check-car-report.herokuapp.com/api/gov';
 
 export const getReport = () => api({url: `${baseUrl}/info`, method: 'POST', requireAuth: false});
