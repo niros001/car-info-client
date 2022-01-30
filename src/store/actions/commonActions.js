27efb1ac18a0962ce40f3cbd959e771e98936eec
niros <niro655@gmail.com>
2022-01-30 17:32:42 +0200

    Remove unused functions & require auth for gov api calls

--- src/store/actions/commonActions.js
+++ src/store/actions/commonActions.js
@@ -1,13 +1,6 @@
 import {makeThunkAsyncActionCreator} from 'redux-toolbelt-thunk';
 import {makeActionCreator} from 'redux-toolbelt';
-import * as Api from '../../services/commonApi';
 import * as govApi from '../../services/govApi';
 
-export const getVehicleInfo = makeThunkAsyncActionCreator('VEHICLE_INFO', Api.getVehicleInfo);
-export const getGarageInfo = makeThunkAsyncActionCreator('GARAGE_INFO', Api.getGarageInfo);
 export const changeLanguage = makeActionCreator('CHANGE_LANGUAGE');
-
-////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////
-
 export const getReport = makeThunkAsyncActionCreator('GET_REPORT_DATA', govApi.getReport);
--- src/store/reducers/commonReducer.js
+++ src/store/reducers/commonReducer.js
@@ -2,8 +2,6 @@ import {makeReducer, makeAsyncReducer} from 'redux-toolbelt';
 import {commonActions} from '../actions';
 
 const commonReducer = {
-    vehicleInfo: makeAsyncReducer(commonActions.getVehicleInfo),
-    garageInfo: makeAsyncReducer(commonActions.getGarageInfo),
     language: makeReducer(commonActions.changeLanguage),
     report: makeAsyncReducer(commonActions.getReport),
 }
