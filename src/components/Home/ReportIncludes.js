f734898cca91efd5e4afafc66d73d0f593076698
niros <niro655@gmail.com>
2022-05-22 16:42:53 +0300

    Redesign ReportIncludes.js

--- src/components/Home/ReportIncludes.js
+++ src/components/Home/ReportIncludes.js
@@ -1,53 +1,62 @@
 import React, {useMemo} from 'react';
 import styled from 'styled-components';
-import {Responsive, container} from '../common';
+import {container} from '../common';
 import includes01Svg from '../../assets/includes01.svg'
 import includes02Svg from '../../assets/includes02.svg'
 import includes03Svg from '../../assets/includes03.svg'
 import includes04Svg from '../../assets/includes04.svg'
 import includes05Svg from '../../assets/includes05.svg'
-import includes06Svg from '../../assets/includes06.svg'
 
 const Container = styled.div`
   ${container};
-  background-color: #1AE5BE;
+  background-color: #555FDE;
+  margin-top: 62.5px;
+  @media(max-width: 540px) {
+    margin-top: unset;
+  }
 `
 
-const Content = styled(Responsive)`
+const Content = styled.div`
+  display: flex;
   flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: space-around;
+  flex-wrap: unset;
+  justify-content: center;
   align-items: center;
+  @media(max-width: 540px) {
+    flex-wrap: wrap;
+  }
 `
 
 const Item = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin: 0 10px;
+  margin-top: -62.5px;
+  @media(max-width: 540px) {
+    margin-top: unset;
+  }
 `
 
 const Icon = styled.img`
-  width: 120px;
-  height: 120px;
+  width: 115px;
+  height: 115px;
 `
 
 const Text = styled.div`
   font-size: 14px;
   font-weight: 500;
-  color: #1F2737;
-  max-width: 120px;
+  color: #FFFFFF;
+  max-width: 90px;
   text-align: center;
   margin-top: -15px;
 `
 const ReportIncludes = ({t}) => {
   const includes = useMemo(() => [
-    {src: includes01Svg, description: t('Impairment test and total loss')},
-    {src: includes02Svg, description: t('Maintenance costs and red flags')},
+    {src: includes05Svg, description: t('Checking the seller vehicle')},
     {src: includes03Svg, description: t('Official mortgage check')},
     {src: includes04Svg, description: t('Vehicle history from database')},
-    {src: includes05Svg, description: t('Checking the seller vehicle')},
-    {src: includes06Svg, description: t('Checking vehicle costs')},
+    {src: includes02Svg, description: t('Maintenance costs and red flags')},
+    {src: includes01Svg, description: t('Impairment test and total loss')},
   ], [t])
 
   return (
