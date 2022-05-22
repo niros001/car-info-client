65b43b83af863ef6ac67267b3906e4ec270e4690
niros <niro655@gmail.com>
2022-05-22 17:51:30 +0300

    Redesign AdditionalInfo.js

--- src/components/Home/AdditionalInfo.js
+++ src/components/Home/AdditionalInfo.js
@@ -13,28 +13,35 @@ const Content = styled(Responsive)`
 
 const Title = styled.div`
   font-size: 20px;
-  font-weight: 500;
-  color: #525252;
+  font-weight: bold;
+  color: #555FDE;
   margin-bottom: 12px;
 `
 
 const Info = styled.div`
   font-size: 15px;
-  font-weight: 400;
-  color: #525252;
+  font-weight: ${({bold}) => bold ? 'bold' : '400'};
+  color: #1F2737;
 `
 
 const AdditionalInfo = ({t}) => (
     <Container style={{color: 'black'}}>
           <Content>
-                <Title>{t('What is check car report')}</Title>
-                <Info>{t('What is check car report info')}</Info>
+                <Title>{t('Start with')}</Title>
+                <Info>{t('Start with info')}</Info>
                 <br />
-                <Title>{t('Sounds revolutionary')}</Title>
-                <Info>{t('Sounds revolutionary info')}</Info>
+                <Title>{t('Check before seen')}</Title>
+                <Info>{t('Check before seen info')}</Info>
                 <br />
-                <Title>{t('It is legal')}</Title>
-                <Info>{t('It is legal info')}</Info>
+                <Title>{t('Vehicle CV')}</Title>
+                <Info>{t('Vehicle CV info')}</Info>
+                <br />
+                <Title>{t('Title 4')}</Title>
+                <Info bold>{t('Info 1')}</Info>
+                <Info bold>{t('Info 2')}</Info>
+                <Info bold>{t('Info 3')}</Info>
+                <Info bold>{t('Info 4')}</Info>
+                <Info bold>{t('Info 5')}</Info>
           </Content>
     </Container>
 )
